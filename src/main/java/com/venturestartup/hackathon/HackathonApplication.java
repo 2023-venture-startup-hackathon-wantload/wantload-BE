eb1c48a5d9cbd08ff3f228fa47ebed13d3f8f7e8
ziiyouth <ziy913@naver.com>
2023-08-29 17:12:36 +0900

    first commit

---
+++ src/main/java/com/venturestartup/hackathon/HackathonApplication.java
@@ -0,0 +1,24 @@
+package com.venturestartup.hackathon;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+	}
+
+}
